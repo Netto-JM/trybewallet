aefa77beda13aa4c707d0c794336c4f641a06087
NettoJM <nettojm86@gmail.com>
2022-12-13 17:14:45 -0300

    configured redux store, created action and reducer for user

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,11 @@
-// Coloque aqui suas actions
+const SAVE_USER = 'SAVE_USER';
+
+const saveUser = (userEmail) => ({
+  type: SAVE_USER,
+  payload: userEmail,
+});
+
+export {
+  SAVE_USER,
+  saveUser,
+};
