0eb8395186975955a575bf5dd697baf689896f9e
NettoJM <nettojm86@gmail.com>
2022-12-13 17:41:24 -0300

    started to build Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
-class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
-  }
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isDisable, setIsDisable] = useState(true);
+
+  const history = useHistory();
+
+  const submitHandle = (event) => {
+    event.preventDefault();
+    history.push('/carteira');
+  };
+
+  useEffect(() => {
+    const MIN_PASS_LENGTH = 6;
+    setIsDisable(password.length < MIN_PASS_LENGTH);
+  }, [email, password]);
+
+  return (
+    <div>
+      <form>
+        <input
+          type="email"
+          placeholder="Email"
+          data-testid="email-input"
+          value={ email }
+          onChange={ ({ target: { value } }) => { setEmail(value); } }
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          data-testid="password-input"
+          value={ password }
+          onChange={ ({ target: { value } }) => { setPassword(value); } }
+        />
+        <button
+          type="submit"
+          disabled={ isDisable }
+          onClick={ submitHandle }
+        >
+          Entrar
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
