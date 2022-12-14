9a22ca37a5345201fc373264577b8aec9343ff8d
NettoJM <nettojm86@gmail.com>
2022-12-14 13:56:54 -0300

    Added wallet reducer

--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,1 +1,41 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import {
+  FETCH_CURRENCIES_STARTED,
+  FETCH_CURRENCIES_SUCCESSFUL,
+  FETCH_CURRENCIES_FAILED,
+} from '../actions';
+
+const INITIAL_STATE = {
+  isFetchingCurrencies: false,
+  errorMessageCurrencies: '',
+  currencies: [],
+};
+
+const wallet = (state = INITIAL_STATE, { type, payload }) => {
+  switch (type) {
+  case FETCH_CURRENCIES_STARTED:
+    return {
+      ...state,
+      isFetchingCurrencies: true,
+      errorMessageCurrencies: '',
+      currencies: [],
+    };
+  case FETCH_CURRENCIES_SUCCESSFUL:
+    return {
+      ...state,
+      isFetchingCurrencies: false,
+      errorMessageCurrencies: '',
+      currencies: payload,
+    };
+  case FETCH_CURRENCIES_FAILED:
+    return {
+      ...state,
+      isFetchingCurrencies: false,
+      errorMessageCurrencies: payload,
+      currencies: [],
+    };
+  default:
+    return state;
+  }
+};
+
+export default wallet;
