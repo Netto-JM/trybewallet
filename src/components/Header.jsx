04471b592e00e5a2438fd2fbb7dc05f0d1d9cfde
NettoJM <nettojm86@gmail.com>
2022-12-13 22:18:22 -0300

    Header component partially completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-class Header extends Component {
-  render() {
-    return (
-      <div>Header</div>
-    );
-  }
+function Header(props) {
+  const { email } = props;
+  return (
+    <header>
+      <span data-testid="email-field">{`Email: ${email}`}</span>
+      <span data-testid="total-field">Despesa Total: R$ 0,00</span>
+      <span data-testid="header-currency-field">BRL</span>
+    </header>
+  );
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
