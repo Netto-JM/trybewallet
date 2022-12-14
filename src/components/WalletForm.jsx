e3b6757472bf1c52eac06b3827a16904b436e946
NettoJM <nettojm86@gmail.com>
2022-12-13 23:58:31 -0300

    started coding the WalletForm and related actions

--- src/components/WalletForm.jsx
+++ src/components/WalletForm.jsx
@@ -1,11 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
 
-class WalletForm extends Component {
-  render() {
-    return (
-      <div>WalletForm</div>
-    );
-  }
+function WalletForm(props) {
+  const [expenseValue, setExpenseValue] = useState('');
+  const [description, setDescription] = useState('');
+
+  const { dispatch } = props;
+
+  useEffect(() => {
+    dispatch();
+  }, [dispatch]);
+
+  return (
+    <form>
+      <label htmlFor="value-input">
+        Valor da despesa:
+        {' '}
+        <input
+          type="number"
+          data-testid="value-input"
+          id="value-input"
+          onChange={ ({ target: { value } }) => { setExpenseValue(value); } }
+          value={ expenseValue }
+        />
+      </label>
+      <label htmlFor="description-input">
+        Descrição da despesa:
+        {' '}
+        <input
+          type="text"
+          data-testid="description-input"
+          id="description-input"
+          onChange={ ({ target: { value } }) => { setDescription(value); } }
+          value={ description }
+        />
+      </label>
+    </form>
+  );
 }
 
-export default WalletForm;
+const mapStateToProps = (state) => ({
+  email: state.user.email, // ainda vou alterar isto
+});
+
+WalletForm.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps)(WalletForm);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,11 +1,36 @@
-const SAVE_USER = 'SAVE_USER';
+export const SAVE_USER = 'SAVE_USER';
+export const FETCH_CURRENCIES_STARTED = 'FETCH_CURRENCIES_STARTED';
+export const FETCH_CURRENCIES_SUCCESSFUL = 'FETCH_CURRENCIES_SUCCESSFUL';
+export const FETCH_CURRENCIES_FAILED = 'FETCH_CURRENCIES_FAILED';
 
-const saveUser = (userEmail) => ({
+export const saveUser = (userEmail) => ({
   type: SAVE_USER,
   payload: userEmail,
 });
 
-export {
-  SAVE_USER,
-  saveUser,
+const fetchCurrenciesStarted = () => ({
+  type: FETCH_CURRENCIES_STARTED,
+});
+
+const fetchCurrenciesSuccessful = (currencies) => ({
+  type: FETCH_CURRENCIES_SUCCESSFUL,
+  payload: currencies,
+});
+
+const fetchCurrenciesFailed = (error) => ({
+  type: FETCH_CURRENCIES_FAILED,
+  payload: error,
+});
+
+export const fetchCurrencies = () => async (dispatch) => {
+  dispatch(fetchCurrenciesStarted());
+  const ENDPOINT = 'https://economia.awesomeapi.com.br/json/all';
+  try {
+    const response = await fetch(ENDPOINT);
+    const data = await response.json();
+    const currencies = Object.keys(data).filter((currency) => currency !== 'USDT');
+    dispatch(fetchCurrenciesSuccessful(currencies));
+  } catch (error) {
+    dispatch(fetchCurrenciesFailed(error));
+  }
 };
