a951ab3bef744a9086db77d1ee9a3d4baf33c1e6
NettoJM <nettojm86@gmail.com>
2022-12-15 21:26:50 -0300

    added a table to show expenses

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,11 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Table extends Component {
-  render() {
-    return (
-      <div>Table</div>
-    );
-  }
+function Table() {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+      </thead>
+    </table>
+  );
 }
 
 export default Table;
