88380ef40d4e5362b491c578644685595ec6ac9a
NettoJM <nettojm86@gmail.com>
2022-12-15 22:22:45 -0300

    added logic to get data from the global state

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-function Table() {
+function Table(props) {
+  const { expenses } = props;
+
+  const expensesTable = expenses
+    .map(({ id, value, description, currency, method, tag, exchangeRates }) => (
+      <tr key={ id }>
+        <td>{ description }</td>
+        <td>{ tag }</td>
+        <td>{ method }</td>
+        <td>{ (+value).toFixed(2) }</td>
+        <td>{ exchangeRates[currency].name }</td>
+        <td>{ (+exchangeRates[currency].ask).toFixed(2) }</td>
+        <td>{ (value * exchangeRates[currency].ask).toFixed(2) }</td>
+        <td>Real</td>
+      </tr>
+    ));
+
   return (
     <table className="table">
       <thead>
@@ -16,8 +34,27 @@ function Table() {
           <th>Editar/Excluir</th>
         </tr>
       </thead>
+      <tbody>{expensesTable}</tbody>
     </table>
   );
 }
 
-export default Table;
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      value: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+      method: PropTypes.string.isRequired,
+      tag: PropTypes.string.isRequired,
+      exchangeRates: PropTypes.shape(PropTypes.shape).isRequired,
+    }),
+  ).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
