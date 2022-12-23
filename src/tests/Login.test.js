3b5db42c7b1349c575076e484c0606655dcb0e19
NettoJM <nettojm86@gmail.com>
2022-12-23 17:05:03 -0300

    code reorganized

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -7,11 +7,12 @@ describe('Testes do componente <Login.jsx />', () => {
   const PASSWORD_TEST_ID = 'password-input';
   it('Testa se a página de Login é carregada assim que a aplicação é renderizada na rota "/"', () => {
     const { history } = renderWithRouterAndRedux(<App />);
+    expect(history.location.pathname).toBe('/');
+
     screen.getByTestId(EMAIL_TEST_ID);
     screen.getByTestId(PASSWORD_TEST_ID);
-    const submitButton = screen.getByRole('button', { name: /entrar/i });
 
+    const submitButton = screen.getByRole('button', { name: /entrar/i });
     expect(submitButton).toBeDisabled();
-    expect(history.location.pathname).toBe('/');
   });
 });
