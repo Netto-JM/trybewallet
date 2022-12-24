858d02266c394c175c1e255314c287cc41c0305f
NettoJM <nettojm86@gmail.com>
2022-12-24 14:33:04 -0300

    added a file for storing constants

---
+++ src/tests/constants.js
@@ -0,0 +1,4 @@
+export const EMAIL_TEST_ID = 'email-input';
+export const PASSWORD_TEST_ID = 'password-input';
+export const VALID_FORMAT_EMAIL = 'test@example.com';
+export const VALID_FORMAT_PASSWORD = 'superSecurePassword';
