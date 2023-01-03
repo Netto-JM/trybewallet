5ac9339b1d6665ba32d3eb12fbcfbf24d4068473
NettoJM <nettojm86@gmail.com>
2023-01-03 17:04:25 -0300

    created new constants

--- src/tests/constants.js
+++ src/tests/constants.js
@@ -1,6 +1,12 @@
 export const EMAIL_INPUT_TEST_ID = 'email-input';
 export const PASSWORD_INPUT_TEST_ID = 'password-input';
 
+export const VALUE_INPUT_TEST_ID = 'value-input';
+export const DESCRIPTION_INPUT_TEST_ID = 'description-input';
+export const CURRENCY_INPUT_TEST_ID = 'currency-input';
+export const METHOD_INPUT_TEST_ID = 'method-input';
+export const TAG_INPUT_TEST_ID = 'tag-input';
+
 export const VALID_FORMAT_EMAIL = 'test@example.com';
 export const VALID_FORMAT_PASSWORD = 'superSecurePassword';
 
